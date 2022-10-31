0cba95e42d8dbbbb13dd85c221797bf93573267a
oneday1302 <kovalchukvladyslav2gmail.com>
2022-10-31 19:37:25 +0100

    added tests on FileReader

---
+++ FormulaOneApplication/src/test/java/ua/foxminded/javaspring/formulaoneapplication/FileReaderTest.java
@@ -0,0 +1,36 @@
+package ua.foxminded.javaspring.formulaoneapplication;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+
+class FileReaderTest {
+
+    @Test
+    void FileReader_shouldReturnIllegalArgumentException_whenInputNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new FileReader(null);
+        });
+    }
+
+    @Test
+    void getData_shouldReturnNullPointerException_whenInputNonExistingFileName() {
+        FileReader fileReader = new FileReader("abc.txt");
+        assertThrows(NullPointerException.class, () -> {
+            fileReader.getData();
+        });
+    }
+    
+    @Test
+    void getData_shouldReturnStream_whenInputCorrectFileName() {
+        List<String> expected = new ArrayList<>();
+        expected.add("Hello");
+        expected.add("World");
+        
+        List<String> actual = new FileReader("test.txt").getData().collect(Collectors.toList());
+        assertEquals(expected, actual);
+    }
+}
